6b469f08d65d18b9478f4ade1443b14c123c9062
Sang Lee <68920478+slee-aws@users.noreply.github.com>
2023-08-23 11:27:05 -0700

    Update SDK to 1.5.1 (#17)

--- pages/index.js
+++ pages/index.js
@@ -500,7 +500,7 @@ export default function Broadcast() {
           <link rel='icon' href='/favicon.ico' />
         </Head>
         <Script
-          src='https://web-broadcast.live-video.net/1.3.3/amazon-ivs-web-broadcast.js'
+          src='https://web-broadcast.live-video.net/1.5.1/amazon-ivs-web-broadcast.js'
           strategy='afterInteractive'
           onLoad={() => {
             const streamConfig = getConfigFromResolution(
